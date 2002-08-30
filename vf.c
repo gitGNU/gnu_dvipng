26700c691bb461d0f1cb53a2201230e73e399b03
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2002-08-30 10:33:38 +0000

    simplify, fix debug info, zap STRSIZE limit

--- vf.c
+++ vf.c
@@ -32,17 +32,13 @@ int32_t SetVF P2C(int32_t, c, int, PassNo)
   while (bytes_left > 0)  {
 #ifdef DEBUG
     if (Debug)
-      printf("\n VF CMD:\t%s ", dvi_commands[*command]);
+      printf("\n  VF MACRO:\t%s ", dvi_commands[*command]);
 #endif
     DrawCommand(command,PassNo);
     length = CommandLength(command);
     command += length;
     bytes_left -= length;
   } /* while  */
-#ifdef DEBUG
-  if (Debug)
-    printf("\n");
-#endif
   --vfstackptr;
   DrawCommand(&pop,PassNo);
   return(ptr->tfmw);
@@ -66,15 +62,15 @@ void InitVF  P1C(struct font_entry *,tfontp)
 
   { /* VF 202 format */
     unsigned char  command[STRSIZE];
-    unsigned char*   current;           
-    int length;
+    unsigned char* lcommand;           
+    int strlength,length;
 
     t = fgetc(tfontp->filep);
     fread(command,1,t,tfontp->filep); /* vf comment */
 #ifdef DEBUG
-    if (Debug) fprintf(ERR_STREAM, "VF COMMENT: %.*s\n", (int)t,command );
+    if (Debug) 
+      printf("(VF_PRE: '%.*s' ", (int)t, command);
 #endif
-
     fread(command,1,8,tfontp->filep);
     t = UNumRead(command, 4);
     CheckChecksum (tfontp->c, t, tfontp->name);
@@ -82,7 +78,7 @@ void InitVF  P1C(struct font_entry *,tfontp)
     tfontp->designsize = UNumRead(command+4,4);
 #ifdef DEBUG
     if (Debug)
-      printf("VF DESIGNSIZE:\t%d\n", tfontp->designsize );
+      printf(")");
 #endif
     tfontp->is_vf = _TRUE;
 
@@ -90,40 +86,29 @@ void InitVF  P1C(struct font_entry *,tfontp)
     tfontp->hfontnump=NULL;
 
     *command = fgetc(tfontp->filep);
-    current = command+1;
-#ifdef DEBUG
-    if (Debug)
-      printf("READ VF FNT@%ld:\t%d\n", 
-	     ftell(tfontp->filep) - 1, *command);
-#endif
     while(*command >= FNT_DEF1 && *command <= FNT_DEF4) {
-      switch (*command)  {
-      case FNT_DEF4:
-	*(current)++ = fgetc(tfontp->filep);
-      case FNT_DEF3: 
-	*(current)++ = fgetc(tfontp->filep);
-      case FNT_DEF2: 
-	*(current)++ = fgetc(tfontp->filep);
-      case FNT_DEF1: 
-	*(current)++ = fgetc(tfontp->filep);
-	if (fread(current,1,14,tfontp->filep) < 14) 
-	  Fatal("vf internal font definition ends prematurely");
-	current += 14;
-	length = *(current-1) + *(current-2);
-	if (length > (int)STRSIZE - 1 - *command + FNT_DEF1 - 1 - 14)
-	  Fatal("vf internal font name longer than internal string size");
-	if (fread(current,1,length,tfontp->filep) < length) 
-	  Fatal("vf internal font name shorter than expected");
-	FontDef(command,tfontp);
-      }
-      *command = fgetc(tfontp->filep);
-      current = command+1;
 #ifdef DEBUG
       if (Debug)
-	printf("READ VF FNT@%ld:\t%d\n", ftell(tfontp->filep) - 1, *command);
+	printf("\n@%ld VF:\t%s ", ftell(tfontp->filep) - 1, 
+	       dvi_commands[*command]);
 #endif
+      length = dvi_commandlength[*command];
+      if (fread(command+1, 1, length-1, tfontp->filep) < length-1) 
+	Fatal("%s command shorter than expected",dvi_commands[*command]);
+      strlength = *(command + length-1) + *(command+length-2);
+      lcommand=command;
+      if (strlength + (uint32_t)length >  (uint32_t)STRSIZE) {
+	/* string + command length exceeds that of buffer */
+	/* fallback, unlikely to ever be used */
+	if ((lcommand=alloca(length+strlength))==NULL) 
+	  Fatal("cannot allocate memory for vf %s",dvi_commands[*command]);
+	memcpy(lcommand,command,length);
+      }
+      if (fread(lcommand+length,1,strlength,tfontp->filep) < strlength) 
+	Fatal("%s command shorter than expected",dvi_commands[*lcommand]);
+      FontDef(lcommand,tfontp);	
+      *command = fgetc(tfontp->filep);
     }
-
     
     /* Read char definitions */
     while(*command < FNT_DEF1) {
@@ -132,91 +117,51 @@ void InitVF  P1C(struct font_entry *,tfontp)
       int bytes_left=0;            /* bytes left in current macro 'batch'*/
       unsigned char *macro=NULL;
       
+#ifdef DEBUG
+      if (Debug)
+	printf("\n@%ld VF CHAR:\t", ftell(tfontp->filep) - 1);
+#endif
       tcharptr=xmalloc(sizeof(struct char_entry));
-      /*#ifdef DEBUG
-	if (Debug)
-	printf("VF CHAR@%d\n", tcharptr);
-	#endif*/
       switch (*command)  {
       case LONG_CHAR:
 	fread(command+1,1,12,tfontp->filep);
-	tcharptr->macro_length = 
-	  macro_length = UNumRead(command+1,4);
+	tcharptr->macro_length = macro_length = UNumRead(command+1,4);
 	if ((c = UNumRead(command+5,4))>NFNTCHARS) /* Only positive for now */
 	  Fatal("vf character exceeds numbering limit");
-#ifdef DEBUG
-	if (Debug)
-	  printf("VF CHAR@%ld:\t%d\n", ftell(tfontp->filep) - 13, c);
-#endif
 	tfontp->ch[c] = tcharptr;
 	tcharptr->tfmw = (int32_t) 
 	  ((int64_t) UNumRead(command+9,4) * tfontp->d / (1 << 20));
-	if (macro_length > MACRO_BATCHSIZE) {
-	  Warning("vf macro too long; ignored");
-	  fseek(tfontp->filep,macro_length,SEEK_CUR);
-	  tcharptr->macro=NULL;
-	  tcharptr->macro_length=0;
-	} else {
-	  if (macro == NULL || bytes_left < macro_length) {
-	    macro=xmalloc(MACRO_BATCHSIZE);
-	    tcharptr->free_me=_TRUE;
-	    bytes_left=MACRO_BATCHSIZE;
-	  } else {
-	    tcharptr->free_me=_FALSE;
-	  }
-	  tcharptr->macro=macro;
-	  if (fread(macro,1,macro_length,tfontp->filep) < macro_length) 
-	    Fatal("vf char ends prematurely");
-	  macro += macro_length;
-	  bytes_left -= macro_length;
-	}
 	break;
       default:
 	fread(command+1,1,4,tfontp->filep);
-	tcharptr->macro_length = 
-	  macro_length = *command;
-	if ((c = *(command+1)) > NFNTCHARS)
+	tcharptr->macro_length = macro_length = UNumRead(command,1);
+	if ((c = UNumRead(command+1,1)) > NFNTCHARS)
 	  Fatal("vf character exceeds numbering limit");
-#ifdef DEBUG
-	if (Debug)
-	  printf("VF CHAR@%ld:\t%d\n", ftell(tfontp->filep) - 5, c);
-#endif
 	tfontp->ch[c] = tcharptr;
 	tcharptr->tfmw = (int32_t) 
 	  ((int64_t) UNumRead(command+2,3) * tfontp->d / (1 << 20));
-	if (macro_length > MACRO_BATCHSIZE) {
-	  Warning("vf macro too long; ignored");
-	  fseek(tfontp->filep,macro_length,SEEK_CUR);
-	  tcharptr->macro=NULL;
-	  tcharptr->macro_length=0;
+      }
+      if (macro_length > MACRO_BATCHSIZE) {
+	Warning("vf macro too long; ignored");
+	fseek(tfontp->filep,macro_length,SEEK_CUR);
+	tcharptr->macro=NULL;
+	tcharptr->macro_length=0;
+      } else {
+	if (macro == NULL || bytes_left < macro_length) {
+	  macro=xmalloc(MACRO_BATCHSIZE);
+	  tcharptr->free_me=_TRUE;
+	  bytes_left=MACRO_BATCHSIZE;
 	} else {
-	  if (macro == NULL || bytes_left < macro_length) {
-	    macro=xmalloc(MACRO_BATCHSIZE);
-	    tcharptr->free_me=_TRUE;
-	    bytes_left=MACRO_BATCHSIZE;
-	  } else {
-	    tcharptr->free_me=_FALSE;
-	  }
-	  tcharptr->macro=macro;
-	  if (fread(macro,1,macro_length,tfontp->filep) < macro_length) 
-	    Fatal("vf char ends prematurely");
-	  macro += macro_length;
-	  bytes_left -= macro_length;
+	  tcharptr->free_me=_FALSE;
 	}
-	break;
+	tcharptr->macro=macro;
+	if (fread(macro,1,macro_length,tfontp->filep) < macro_length) 
+	  Fatal("vf char ends prematurely");
+	macro += macro_length;
+	bytes_left -= macro_length;
       }
       *command = fgetc(tfontp->filep);
-      current = command+1;
     }
   }
-  /*#ifdef DEBUG
-  if (Debug)
-    printf("VF FONTNUMP@%d\n", tfontp->hfontnump);
-#endif
-#ifdef DEBUG
-  if (Debug)
-    printf("VF FONT@%d\n", tfontp);
-#endif
-  */
 }
 
