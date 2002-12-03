1c1175fa3da556429b5a2e2de83ed4f4d70f9354
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2002-12-03 08:15:25 +0000

    autoconfiscate

--- dvipng.c
+++ dvipng.c
@@ -1,5 +1,4 @@
-/* dvipng.c
- */
+/* dvipng.c */
 /**********************************************************************
  ****************************  Intro  *********************************
  **********************************************************************
@@ -32,21 +31,24 @@ int main(int argc, char ** argv)
   bool parsestdin;
   
 #ifdef TIMING
-#ifdef BSD_TIME_CALLS
-  ftime(&timebuffer);
-  timer = timebuffer.time + (float)(timebuffer.millitm) / 1000.0;
-#else
+# ifdef HAVE_GETTIMEOFDAY
   gettimeofday(&Tp, NULL);
   timer = Tp.tv_sec + ((float)(Tp.tv_usec))/ 1000000.0;
+# else
+#  ifdef HAVE_FTIME
+  ftime(&timebuffer);
+  timer = timebuffer.time + (float)(timebuffer.millitm) / 1000.0;
+#  else
+  timer = 0;
+#  endif
+# endif
 #endif
-#endif
-
-  setbuf(ERR_STREAM, NULL);
+  //  setbuf(ERR_STREAM, NULL);
 
   initcolor();
-  parsestdin=DecodeArgs(argc, argv);
+  parsestdin = DecodeArgs(argc, argv);
 
-#ifdef KPATHSEA
+#ifdef HAVE_LIBKPATHSEA
   kpse_set_progname(argv[0]);
   kpse_set_program_enabled (kpse_pk_format, makeTexPK, kpse_src_compile);
   kpse_init_prog("DVIPNG", resolution, MFMODE, "cmr10");
@@ -72,23 +74,23 @@ int main(int argc, char ** argv)
   }
 
 #ifdef TIMING
-#ifdef BSD_TIME_CALLS
-  ftime(&timebuffer);
-  timer = (timebuffer.time + (float)(timebuffer.millitm)/1000.0) - timer;
-#else
+# ifdef HAVE_GETTIMEOFDAY
   gettimeofday(&Tp, NULL);
   timer = (Tp.tv_sec + (float)(Tp.tv_usec)/1000000.0) - timer;
-#endif
+# else
+#  ifdef HAVE_FTIME
+  ftime(&timebuffer);
+  timer = (timebuffer.time + (float)(timebuffer.millitm)/1000.0) - timer;
+#  endif
+# endif
   
-  if (ndone > 0) {
+  if (ndone > 0) 
     fprintf(ERR_STREAM,
 	    "Time of complete run: %.2f s, %d page(s), %.2f s/page.\n",
 	    timer, ndone, timer / ndone);
-  }
-  if (my_toc >= 0.01) {
+  if (my_toc >= 0.01) 
     fprintf(ERR_STREAM,
 	    "Thereof in TIC/TOC region %.2f s.\n",my_toc);
-  }
 #endif
 
   exit(EXIT_SUCCESS);
